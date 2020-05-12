c72956de9834c217be86b03d9219e7bf935765c0
amrl <amirul.ardy@gmail.com>
2020-05-13 01:30:56 +0800

    Start scanning when app opens

---
+++ app/src/main/java/me/amrl/pqrs/ScanActivity.java
@@ -0,0 +1,11 @@
+package me.amrl.pqrs;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * Acts as the activity used when scanning QR codes.
+ * Mainly used to set scanning orientation to portrait only.
+ * See AndroidManifest.xml.
+ */
+public class ScanActivity extends CaptureActivity {
+}
